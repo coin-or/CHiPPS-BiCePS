4a147ead64a12a1784c795323dd4f803919550f3
Yan Xu <sasyanxu@yahoo.com>
2007-04-16 23:59:51 +0000

    add

---
+++ Bcps/src/Bcps.h
@@ -0,0 +1,27 @@
+/*===========================================================================*
+ * This file is part of the Branch, Constrain and Price Software (BiCePS)    *
+ *                                                                           *
+ * BiCePS is distributed under the Common Public License as part of the      *
+ * COIN-OR repository (http://www.coin-or.org).                              *
+ *                                                                           *
+ * Authors: Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
+ *          Matthew Saltzman, Clemson Unniversity                            *
+ *                                                                           *
+ * Copyright (C) 2001-2007, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ * All Rights Reserved.                                                      *
+ *===========================================================================*/
+
+#ifndef Bcps_h_
+#define Bcps_h_
+
+//#############################################################################
+
+const int BCPS_CONSTRAINT  = 10;
+const int BCPS_VARIABLE  = 11;
+
+//#############################################################################
+
+#endif
+
---
+++ Bcps/src/BcpsModel.cpp
@@ -0,0 +1,80 @@
+/*===========================================================================*
+ * This file is part of the Branch, Constrain and Price Software (BiCePS)    *
+ *                                                                           *
+ * BiCePS is distributed under the Common Public License as part of the      *
+ * COIN-OR repository (http://www.coin-or.org).                              *
+ *                                                                           *
+ * Authors: Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
+ *          Matthew Saltzman, Clemson Unniversity                            *
+ *                                                                           *
+ * Copyright (C) 2001-2007, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ * All Rights Reserved.                                                      *
+ *===========================================================================*/
+
+#include "Bcps.h"
+#include "BcpsModel.h"
+
+//#############################################################################
+
+AlpsReturnCode 
+BcpsModel::encodeBcps(AlpsEncoded *encoded) const
+{
+    AlpsReturnCode status  = ALPS_OK;
+    int i, size = 0;
+    
+    size = constraints_.size();
+    encoded->writeRep(size);
+    for (i = 0; i < size; ++i) {
+        constraints_[i]->encode(encoded); 
+    }
+    
+    size =  variables_.size();
+    encoded->writeRep(size);
+    for (i = 0; i < size; ++i) {
+        variables_[i]->encode(encoded);
+    }
+    
+    // Core node decription
+    encoded->writeRep(numCoreConstraints_);
+    encoded->writeRep(numCoreVariables_);
+    
+    return status;
+}
+
+//#############################################################################
+
+AlpsReturnCode 
+BcpsModel::decodeBcps(AlpsEncoded &encoded)
+{
+    AlpsReturnCode status  = ALPS_OK;
+    int i, size;
+    
+    encoded.readRep(size);
+    for (i = 0; i < size; ++i) {
+        BcpsConstraint *con = static_cast<BcpsConstraint *>
+            ( broker_->decoderObject("BCPS_CONSTRAINT")->decode(encoded) );
+
+        constraints_.push_back(con);
+        con = NULL;
+    }
+
+    encoded.readRep(size);
+    for (i = 0; i < size; ++i) {
+        BcpsVariable *var = static_cast<BcpsVariable *>
+            ( broker_->decoderObject("BCPS_VARIABLE")->decode(encoded) );
+
+        variables_.push_back(var);
+        var = NULL;
+    }
+    
+    // Core node decription
+    encoded.readRep(numCoreConstraints_);
+    encoded.readRep(numCoreVariables_);
+
+    return status;
+}
+
+//#############################################################################
+
