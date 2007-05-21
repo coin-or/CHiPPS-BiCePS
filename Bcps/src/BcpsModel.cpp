235dddd477801a0309ac7e2f86037db53d0bed1a
Ted Ralphs <ted@lehigh.edu>
2007-05-21 17:00:06 +0000

    More updates to copyright statements, README, and AUTHORS files

--- Bcps/src/BcpsModel.cpp
+++ Bcps/src/BcpsModel.cpp
@@ -4,10 +4,17 @@
  * BiCePS is distributed under the Common Public License as part of the      *
  * COIN-OR repository (http://www.coin-or.org).                              *
  *                                                                           *
- * Authors: Yan Xu, Lehigh University                                        *
+ * Authors:                                                                  *
+ *                                                                           *
+ *          Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *                                                                           *
+ * Conceptual Design:                                                        *
+ *                                                                           *
+ *          Yan Xu, Lehigh University                                        *
  *          Ted Ralphs, Lehigh University                                    *
  *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
- *          Matthew Saltzman, Clemson Unniversity                            *
+ *          Matthew Saltzman, Clemson University                             *
  *                                                                           *
  * Copyright (C) 2001-2007, Lehigh University, Yan Xu, and Ted Ralphs.       *
  * All Rights Reserved.                                                      *
