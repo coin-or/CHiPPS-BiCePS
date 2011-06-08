a81d4eb559ecaa72b6f1216b051dc24643b6d938
Ted Ralphs <ted@lehigh.edu>
2011-06-08 16:48:15 +0000

    Converting to new build system and new header file system

---
+++ Bcps/src/config_bcps_default.h
@@ -0,0 +1,39 @@
+/*===========================================================================*
+ * This file is part of the Abstract Library for Parallel Search (ALPS).     *
+ *                                                                           *
+ * ALPS is distributed under the Common Public License as part of the        *
+ * COIN-OR repository (http://www.coin-or.org).                              *
+ *                                                                           *
+ * Authors:                                                                  *
+ *                                                                           *
+ *          Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *                                                                           *
+ * Conceptual Design:                                                        *
+ *                                                                           *
+ *          Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
+ *          Matthew Saltzman, Clemson University                             *
+ *                                                                           * 
+ *                                                                           *
+ * Copyright (C) 2001-2010, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ *===========================================================================*/
+
+/***************************************************************************/
+/*           HERE DEFINE THE PROJECT SPECIFIC PUBLIC MACROS                */
+/*    These are only in effect in a setting that doesn't use configure     */
+/***************************************************************************/
+
+/* Version number of project */
+#undef BCPS_VERSION "trunk"
+
+/* Major Version number of project */
+#undef BCPS_VERSION_MAJOR   9999
+
+/* Minor Version number of project */
+#undef BCPS_VERSION_MINOR   9999
+
+/* Release Version number of project */
+#undef BCPS_VERSION_RELEASE 9999
+
---
+++ Bcps/src/config_default.h
@@ -0,0 +1,57 @@
+/*===========================================================================*
+ * This file is part of the Abstract Library for Parallel Search (ALPS).     *
+ *                                                                           *
+ * ALPS is distributed under the Common Public License as part of the        *
+ * COIN-OR repository (http://www.coin-or.org).                              *
+ *                                                                           *
+ * Authors:                                                                  *
+ *                                                                           *
+ *          Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *                                                                           *
+ * Conceptual Design:                                                        *
+ *                                                                           *
+ *          Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
+ *          Matthew Saltzman, Clemson University                             *
+ *                                                                           * 
+ *                                                                           *
+ * Copyright (C) 2001-2010, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ *===========================================================================*/
+
+/* include the COIN-OR-wide system specific configure header */
+#include "configall_system.h"
+
+/* include the public project specific macros */
+#include "config_alps_default.h"
+
+/***************************************************************************/
+/*             HERE DEFINE THE PROJECT SPECIFIC MACROS                     */
+/*    These are only in effect in a setting that doesn't use configure     */
+/***************************************************************************/
+
+/* Define to the debug sanity check level (0 is no test) */
+#define COIN_BCPS_CHECKLEVEL 0
+
+/* Define to the debug verbosity level (0 is no output) */
+#define COIN_BCPS_VERBOSITY 0
+
+/* Define to 1 if the ALPS package is used */
+#define COIN_HAS_ALPS 1
+
+/* Define to 1 if the BiCePS package is used */
+#define COIN_HAS_BCPS 1
+
+/* Define to 1 if the CoinUtils package is used */
+#define COIN_HAS_COINUTILS 1
+
+/* Define to 1 if the Clp package is used */
+#define COIN_HAS_CLP 1
+
+/* Define to 1 if the Mpi package is used */
+/* #define COIN_HAS_MPI 1 */
+
+/* Define to 1 if readline is available */
+/* #undef COIN_HAS_READLINE */
+
