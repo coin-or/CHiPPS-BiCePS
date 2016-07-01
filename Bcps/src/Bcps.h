19e3c83c33a9f218f2fd0c3a690347d951de5149
Aykut Bulut <aykutblt@gmail.com>
2016-06-30 20:15:24 -0400

    Mainpage to doxygen documentation is added.

--- Bcps/src/Bcps.h
+++ Bcps/src/Bcps.h
@@ -23,6 +23,25 @@
 #ifndef Bcps_h_
 #define Bcps_h_
 
+//! \page handle BcpsMainpage
+
+/*! \mainpage
+
+  BiCePS (Branch, Constrain, and Price Software) is an abstract library build
+  on top of Alps. Alps does not have any assumption about the tree search
+  algorithm. Well BiCePS does. It assumes we are building Branch and Cut type
+  algorithm. It provides the means for this.
+
+  BiCePS assuming branch and bound is also very flexible. It only assumes
+  this, it does not assume much, hence a flexible library. It does not have an
+  assumption about the underlying optimization problem.
+
+  BLIS (BiCePS Linear Integer Solver) is built on top of BiCePS to solve MILP
+  problems. Similarly DisCO (Discrete Conic Optimization) solver is build
+  on top of BiCePS to solve second order conic optimization problems.
+
+ */
+
 //#############################################################################
 // Return code.
 //#############################################################################
