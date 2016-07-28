49879db0b841d7074add2afd328c92e730998014
Aykut Bulut <aykutblt@gmail.com>
2016-07-28 01:01:12 -0400

    Format updates.

--- Bcps/src/BcpsMessage.cpp
+++ Bcps/src/BcpsMessage.cpp
@@ -20,57 +20,64 @@
  * All Rights Reserved.                                                      *
  *===========================================================================*/
 
-#include "BcpsMessage.h"
 #include <cstring>
+#include <map>
 
-//#############################################################################
+#include "Bcps.h"
+#include "BcpsMessage.h"
+
 
 typedef struct {
-    BCPS_Message internalNumber;
-    int externalNumber;              // or continuation
-    char detail;
-    const char * message;
+  BCPS_Message internalNumber;
+  int externalNumber;              // or continuation
+  char detail;
+  const char * message;
 } Bcps_message;
 
-//#############################################################################
 
-static Bcps_message us_english[]=
-{
-    {BCPS_DUMMY_END, 999999, 0, ""}
+static Bcps_message us_english[] = {
+  // tree node
+  {BCPS_NODE_BRANCHONINT, 9201, 1, "Branched on integer variable. Variable index %d."},
+  {BCPS_NODE_UNEXPECTEDSTATUS,9202,1, "Unexpected node status %d"},
+  // grumpy messages
+  // time, node status, node id, parent id, branch direction, obj val [,sum
+  // of column infeasibilities, count of infeasible cols]
+  {BCPS_GRUMPY_MESSAGE_LONG, 900, BCPS_DLOG_GRUMPY, "%.6f %s %d %d %c %.6f %.6f %d"},
+  {BCPS_GRUMPY_MESSAGE_MED, 900, BCPS_DLOG_GRUMPY, "%.6f %s %d %d %c %.6f"},
+  {BCPS_GRUMPY_MESSAGE_SHORT, 900, BCPS_DLOG_GRUMPY, "%.6f %s %d %d %c"},
+  // end of messages
+  {BCPS_DUMMY_END, 999999, 0, ""}
 };
 
-//#############################################################################
 
 /* Constructor */
 BcpsMessage::BcpsMessage(Language language)
-    :
-    CoinMessages(sizeof(us_english) / sizeof(Bcps_message))
-{
-    language_ = language;
-    strcpy(source_, "Bcps");
-    Bcps_message * message = us_english;
+  : CoinMessages(sizeof(us_english) / sizeof(Bcps_message)) {
+  language_ = language;
+  strcpy(source_, "Bcps");
+  Bcps_message * message = us_english;
 
+  while (message->internalNumber != BCPS_DUMMY_END) {
+    CoinOneMessage oneMessage(message->externalNumber, message->detail,
+                              message->message);
+    addMessage(message->internalNumber, oneMessage);
+    message++;
+  }
+
+  // now override any language ones
+
+  switch (language) {
+
+  default:
+    message = NULL;
+    break;
+  }
+
+  // replace if any found
+  if (message) {
     while (message->internalNumber != BCPS_DUMMY_END) {
-        CoinOneMessage oneMessage(message->externalNumber, message->detail,
-                                  message->message);
-        addMessage(message->internalNumber, oneMessage);
-        message++;
+      replaceMessage(message->internalNumber, message->message);
+      message++;
     }
-
-    // now override any language ones
-
-    switch (language) {
-
-    default:
-        message = NULL;
-        break;
-    }
-
-    // replace if any found
-    if (message) {
-        while (message->internalNumber != BCPS_DUMMY_END) {
-            replaceMessage(message->internalNumber, message->message);
-            message++;
-        }
-    }
+  }
 }
