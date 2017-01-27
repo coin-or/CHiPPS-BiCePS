fe3c1d4dc80fee12258e865d0e4512fce94681fe
Ted Ralphs <ted@lehigh.edu>
2017-01-27 23:00:23 +0000

    Updating Copyright statements

--- Bcps/src/BcpsLicense.h
+++ Bcps/src/BcpsLicense.h
@@ -16,6 +16,6 @@
  *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
  *          Matthew Saltzman, Clemson University                             *
  *                                                                           *
- * Copyright (C) 2001-2015, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ * Copyright (C) 2001-2017, Lehigh University, Yan Xu, and Ted Ralphs.       *
  * All Rights Reserved.                                                      *
  *===========================================================================*/
